074f5ffd5c609c8735c6ec02d96f851a5fec8bcf
Lucas <lucas@alternativainternet.com>
2019-08-05 21:52:33 -0300

    creation of user and encrypt of password

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,9 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
-// const User = require('./app/models/User');
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ Message: 'Hello World' }));
-
-// routes.get('/', async (req, res) => {
-//   const user = await User.create({
-//     name: 'Lucas Cruz',
-//     email: 'lucascruzati@gmail.com',
-//     password_hash: '123',
-//     provider: true,
-//   });
-
-//   return res.json(user);
-// });
+routes.post('/users', UserController.store);
 
 export default routes;
