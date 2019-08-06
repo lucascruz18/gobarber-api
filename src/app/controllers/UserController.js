bffd3aa919302821e469415c79740510ccaeaac9
Lucas <lucas@alternativainternet.com>
2019-08-05 23:53:25 -0300

    added middlewares of users and update user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -12,6 +12,33 @@ class UserController {
 
     return res.json(user);
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExist = await User.findOne({ where: { email: email } });
+
+      if (userExist) {
+        return res.status(400).json({ error: 'User already exist.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
 }
 
 export default new UserController();
