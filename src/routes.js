7cf757e9175b3af128cef87fa1df94ef62ad3a90
Lucas <lucas@alternativainternet.com>
2019-08-06 10:57:01 -0300

    service scheduling

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentsController from './app/controllers/AppointmentsController';
 
 import authMiddlewares from './app/middlewares/auth';
 
@@ -19,6 +20,8 @@ routes.use(authMiddlewares);
 
 routes.put('/user', UserController.update);
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentsController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
