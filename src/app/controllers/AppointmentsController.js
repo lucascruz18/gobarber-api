370011e3e66920a04116245f563600203b92f3d5
Lucas <lucas@alternativainternet.com>
2019-08-10 18:19:59 -0300

    virtual fields

--- src/app/controllers/AppointmentsController.js
+++ src/app/controllers/AppointmentsController.js
@@ -14,12 +14,9 @@ class AppointmentsController {
     const { page = 1 } = req.query;
 
     const appointment = await Appointments.findAll({
-      where: {
-        user_id: req.userId,
-        canceled_at: null,
-      },
+      where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 20,
       offset: (page - 1) * 20,
       include: [
